75e3a51eea905368819558dbadfdf8eb0100852a
Elva Plazola <elvaplazola@yahoo.com>
2023-05-03 20:30:10 -0700

    Final Edit

--- index.js
+++ index.js
@@ -12,20 +12,31 @@ let waterIsOn = true
 //Ride 1 works when:
     // green wire is connected
 ride1PowerOn = false
-
+if (greenWireConnected){
+    ride1PowerOn = true
+}
 //Ride 2 works when 
     //green and yellow wires are connected
 ride2PowerOn = false
+if (greenWireConnected && yellowWireConnected){
+    ride2PowerOn = true
+}
 
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
 ride3PowerOn = false
-
+if (greenWireConnected && redWireConnected &&waterIsOn){
+    ride3PowerOn = true 
+}
 //Ride 4 has power when:
-    //ride wire is connected
+    //red wire is connected
 ride4PowerOn = false
+if (redWireConnected){
+    ride4PowerOn = true
+}
+
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
@@ -34,6 +45,24 @@ ride4PowerOn = false
     //if a 5 is rolled it should return "bowling ball"
     //if a 6 is rolled it should return "tiger"
     //if a 7 is rolled it should return "seven"
+
 function ride4SlotMachine(rolledNumber){
-    return "nothing"
+  switch(rolledNumber){
+    case 1: 
+    console.log('star');
+    case 2: 
+    console.log('car');
+    case 3: 
+    console.log('pot of gold');
+    case 4: 
+    console.log('clover');
+    case 5: 
+    console.log('bowling ball');
+    case 6: 
+    console.log('tiger');
+    case 7: 
+    console.log('seven');
+    default:
+        break; 
+  }
 }
